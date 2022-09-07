dd88ede91a5f9077d8cef61f6a84deb452d4b1af
cristian.perez <cristian.perez@infosel.com>
2022-09-07 16:03:59 -0500

    feat: :art: Installing material UI and starting to test components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+
+import logo from './logo.svg'
+import CountrySelect from './components/CountrySelect'
+
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Container maxWidth="sm">
+        <Box>
+          <CountrySelect />
+          <TextField id="outlined-basic" label="ZipCode" variant="outlined" />
+          <Button color='primary' variant="contained">Submit</Button>
+        </Box>
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
