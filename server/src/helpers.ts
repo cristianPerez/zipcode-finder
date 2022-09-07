45f8bd331076fbed48d2f82320f8b35d019360c5
cristian.perez <cristian.perez@infosel.com>
2022-09-07 14:55:17 -0500

    feat: :art: Helper to request the zippopotam api

---
+++ server/src/helpers.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+// TODO add to a config file and env vars
+const BASE_URL = 'https://api.zippopotam.us'
+
+export const getSearch = async (zipcode: string, countryAbb: string) => {
+  try {
+    const { data } = await axios({
+      method: 'GET',
+      url: `${BASE_URL}/${countryAbb}/${zipcode}`,
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+    const place = data.places.shift()
+    return {
+      zipcode: data['post code'],
+      country: data?.country,
+      city: place['place name'],
+      state: place?.state,
+    }
+  } catch (error) {
+    // TODO check the status of the request
+    throw new Error('Not Found')
+  }
+}
