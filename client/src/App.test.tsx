0963b9c6e6281196865debb79fdb9c168cf719b9
cristian.perez <cristian.perez@infosel.com>
2022-09-07 19:58:12 -0500

    test: :white_check_mark: New test in app

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('It should renders the title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const typography = screen.getByText('We just need your ziocode');
+  expect(typography).toBeInTheDocument();
 });
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import './App.css'
 import logo from './logo.svg'
 import { client } from './graphql/apolloclient'
 import SearchBar from './components/SearchBar'
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <Typography variant="h5">We just need your ziocode</Typography>
         </header>
         <SearchBar />
       </div>
